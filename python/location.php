293f1f042de8643c47afec173303dff956e41a52
Maharavo1 <hei.maharavo@gmail.com>
2025-05-13 14:58:26 +0300

    feat: Add LocationService Class

--- python/location.php
+++ python/location.php
@@ -20,3 +20,37 @@ class Bien:
     def __str__(self):
         return f"{self.type.capitalize()}{{nom='{self.nom}', etat={self.etat}}}"
 
+
+class LocationService:
+    def __init__(self, biens):
+        self.biens = {b.nom: b for b in biens}
+
+    def reserver_bien(self, nom):
+        bien = self._get_bien(nom, False)
+        bien.reserver()
+
+    def annuler_reservation(self, nom):
+        bien = self._get_bien(nom, True)
+        bien.annuler_reservation()
+        print("✔️ Annulation de réservation avec succès")
+
+    def afficher_tous_les_biens(self):
+        print("\nListe des biens :")
+        for bien in self.biens.values():
+            print(bien)
+
+    def afficher_biens_disponibles(self):
+        print("\nBiens disponibles :")
+        for bien in self.biens.values():
+            if bien.est_disponible():
+                print(bien)
+
+    def _get_bien(self, nom, pour_annulation):
+        bien = self.biens.get(nom)
+        if not bien:
+            if pour_annulation:
+                raise Exception("Vous avez annulé une réservation d’un bien qui n'existe pas.")
+            else:
+                raise Exception(f"Le bien '{nom}' n'existe pas.")
+        return bien
+
