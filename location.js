2ab9deec8c8d9969f921b224b96d96580dc95b5f
Maharavo1 <hei.maharavo@gmail.com>
2025-05-13 14:15:35 +0300

    feat: Add reservation and cancellation tests

--- location.js
+++ location.js
@@ -66,3 +66,36 @@ class LocationService {
         return bien;
     }
 }
+
+// Test
+const biens = [
+    new Bien("Maison_Tana", "maison"),
+    new Bien("Toyota123", "voiture"),
+    new Bien("YamahaX", "moto"),
+];
+
+const service = new LocationService(biens);
+
+function executer(action) {
+    try {
+        action();
+    } catch (e) {
+        console.error(e.message);
+    }
+}
+
+executer(() => service.reserverBien("Maison_Tana"));
+executer(() => service.reserverBien("Toyota123"));
+executer(() => service.reserverBien("YamahaX"));
+
+executer(() => service.reserverBien("Maison_Tana"));
+executer(() => service.reserverBien("Toyota123"));
+executer(() => service.reserverBien("YamahaX"));
+
+executer(() => service.annulerReservation("Maison_Tana"));
+executer(() => service.annulerReservation("Toyota123"));
+executer(() => service.annulerReservation("YamahaX"));
+
+executer(() => service.annulerReservation("Inconnu"));
+executer(() => service.annulerReservation("Daba"));
+executer(() => service.annulerReservation("Vaika"));
